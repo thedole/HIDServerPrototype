52e994b78b3d97e456a7f0120296e428bdfe4f18
Dag Ole Kristoffersen <thedole@gmail.com>
2015-12-08 01:15:36 +0100

    Now enumerates all devices

--- HIDServer.Prototype/js/default.js
+++ HIDServer.Prototype/js/default.js
@@ -1,28 +1,49 @@
 ﻿// For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
-	"use strict";
+    "use strict";
 
-	var app = WinJS.Application;
-	var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application,
+        activation = Windows.ApplicationModel.Activation,
+        Devices = Windows.Devices,
+        DeviceEnumeration = Devices.Enumeration;
 
-	app.onactivated = function (args) {
-		if (args.detail.kind === activation.ActivationKind.launch) {
-			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-				// TODO: This application has been newly launched. Initialize your application here.
-			} else {
-				// TODO: This application was suspended and then terminated.
-				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
-			}
-			args.setPromise(WinJS.UI.processAll());
-		}
-	};
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize your application here.
+            } else {
+                // TODO: This application was suspended and then terminated.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+            }
+            args.setPromise(WinJS.UI.processAll().then(function(args) {
+                DeviceEnumeration.DeviceInformation.findAllAsync(DeviceEnumeration.DeviceClass.all).done(function (devs) {
+                    var len = devs.length,
+                        deviceInfo,
+			            line = [],
+                        outputList = document.getElementById('output'),
+                        li;
 
-	app.oncheckpoint = function (args) {
-		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
-	};
+                    for (var i = len - 1; i >= 0; i--) {
+                        deviceInfo = devs[i];
+                        line.push(deviceInfo.name);
+                        line.push(deviceInfo.kind);
 
-	app.start();
+                        li = document.createElement('li');
+                        li.innerText = line.join(' - ');
+                        outputList.appendChild(li);
+                        line = [];
+                    }
+                });
+            }));
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+    };
+
+    app.start();
 })();
