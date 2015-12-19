dbe2e21b66bcd1dde6673e1d3768e6f810bbde4d
Dag Ole Kristoffersen <thedole@gmail.com>
2015-12-19 01:44:06 +0100

    now uses listview to show list of devices

--- HIDServer.Prototype/js/default.js
+++ HIDServer.Prototype/js/default.js
@@ -4,9 +4,7 @@
     "use strict";
 
     var app = WinJS.Application,
-        activation = Windows.ApplicationModel.Activation,
-        Devices = Windows.Devices,
-        DeviceEnumeration = Devices.Enumeration;
+        activation = Windows.ApplicationModel.Activation;
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
@@ -17,24 +15,6 @@
                 // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
             }
             args.setPromise(WinJS.UI.processAll().then(function(args) {
-                DeviceEnumeration.DeviceInformation.findAllAsync(DeviceEnumeration.DeviceClass.all).done(function (devs) {
-                    var len = devs.length,
-                        deviceInfo,
-			            line = [],
-                        outputList = document.getElementById('output'),
-                        li;
-
-                    for (var i = len - 1; i >= 0; i--) {
-                        deviceInfo = devs[i];
-                        line.push(deviceInfo.name);
-                        line.push(deviceInfo.kind);
-
-                        li = document.createElement('li');
-                        li.innerText = line.join(' - ');
-                        outputList.appendChild(li);
-                        line = [];
-                    }
-                });
             }));
         }
     };
---
+++ HIDServer.Prototype/js/devicedata.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    "use strict";
+
+    var deviceInfos = [],
+        listView = new WinJS.Binding.List(deviceInfos),
+        Devices = Windows.Devices,
+        DeviceEnumeration = Devices.Enumeration;
+
+    WinJS.Namespace.define("HIDServer.Prototype.Devices", { list: listView });
+
+
+    DeviceEnumeration.DeviceInformation.findAllAsync(DeviceEnumeration.DeviceClass.all).done(function (devs) {
+        var len = devs.length;
+        for (var i = len - 1; i >= 0; i--) {
+            listView.push({ name: devs[i].name, kind: devs[i].kind });
+        }
+    });
+})()
